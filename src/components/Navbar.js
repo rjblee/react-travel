b068128e642c23323cb99ceb0db98f390ffcee29
Ralph Lee <ralph.lee@live.ca>
2020-11-29 23:50:31 +0900

    Close mobile menu when clicked on logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,7 +27,7 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
           TRVL <i className="fab fa-typo3"></i>
         </Link>
         <div className="menu-icon" onClick={handleClick}>
