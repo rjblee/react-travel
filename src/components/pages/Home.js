b6cc826dd38a1fc39c022b20a9cbb527b07fc0aa
Ralph Lee <ralph.lee@live.ca>
2020-12-13 21:29:52 +0900

    Create Home component

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import { Router, Switch } from "react-router-dom";
+import React from "react";
+import "../../App.css";
+import HeroSection from "../HeroSection";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
+
+export default Home;
